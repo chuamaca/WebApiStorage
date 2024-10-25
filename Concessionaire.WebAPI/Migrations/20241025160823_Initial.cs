94c607af5cadce83a4b9378f3c6283749b52ebc9
Cesar Huamani <153028879+ccastrocsti@users.noreply.github.com>
2024-10-25 12:03:24 -0500

    Add files via upload

---
+++ Concessionaire.WebAPI/Migrations/20241025160823_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concessionaire.WebAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    Model = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    ImagePath = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    TechnicalDataSheetPath = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
